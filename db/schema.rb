d35b2a8423867b7a99c273f9401f2efe1781456f
Ryan Lonergan <ryan.tlonergan@gmail.com>
2017-10-17 20:44:45 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 4) do
+ActiveRecord::Schema.define(version: 5) do
 
   create_table "costume_stores", force: :cascade do |t|
     t.string "name"
     t.string "location"
-    t.integer "cosumte_invetory"
+    t.integer "costume_inventory"
     t.string "num_of_employees"
     t.boolean "still_in_business"
     t.datetime "opening_time"
